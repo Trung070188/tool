efcb66328006c565a2a027c26106887b1132617e
Trung <trung.nguyen@blueocean.net.vn>
2023-05-09 20:52:08 +0700

    fix:update tool

--- app/Console/Commands/TestCommand.php
+++ app/Console/Commands/TestCommand.php
@@ -3,8 +3,12 @@
 namespace App\Console\Commands;
 
 use App\Services\FakeInstallService;
+use GuzzleHttp\Client;
 use Illuminate\Console\Command;
 use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
+use Symfony\Component\DomCrawler\Crawler;
 
 class TestCommand extends Command
 {
@@ -41,7 +45,32 @@ class TestCommand extends Command
      */
     public function handle()
     {
-        dd(get_percent(100,20));
+        $url = 'https://www.etsy.com/listing/1063329364/reading-corner-decor-sign-posterboho?click_key=bd11fb0424cf60cc6661514800b6a146d7ca0eb4%3A1063329364&click_sum=543ea081&ref=shop_home_active_8&pro=1';
+        $response = Http::get($url);
+        $html = $response->body();
+
+        $crawler = new Crawler($html);
+        $images = $crawler->filter('.wt-list-unstyled.wt-display-flex-xs.wt-order-xs-1.wt-flex-direction-column-xs.wt-align-items-flex-end')->filter('li')->each(function (Crawler $node)
+        {
+            $img = $node->filter('img')->attr('data-src-delay');
+            return [
+                'link_img'=> $img
+            ];
+        });
+        $data = [];
+        foreach ($images as $image) {
+            $image['link_img'] = str_replace('il_75x75', 'il_fullxfull', $image['link_img']);
+            $data [] = $image;
+        }
+        dd($data);
+        foreach ($data as $key => $da)
+        {
+            $client = new Client();
+            $response = $client->get($da['link_img']);
+            $content = $response->getBody()->getContents();
+            Storage::put("public/images/img-{{$key}}", $content);
+
+        }
 
     }
 
