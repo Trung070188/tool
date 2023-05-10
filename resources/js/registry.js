3a7f2fedf18141e138a08b345d42a57766b8703f
Trung <trung.nguyen@blueocean.net.vn>
2023-05-10 15:33:02 +0700

    fix:update tool

--- resources/js/registry.js
+++ resources/js/registry.js
@@ -1,61 +1,14 @@
 import DashboardIndex from "./admin/dashboard/DashboardIndex";
-import CampaignForm from "./admin/campaigns/CampaignForm";
-import CampaignIndex from "./admin/campaigns/CampaignIndex";
-import CampaignStatistical from "./admin/campaigns/CampaignStatistical.vue";
-import CampaignStatisticalDetail from "./admin/campaigns/CampaignStatisticalDetail.vue";
-import DeleteCampaign from "./admin/campaigns/DeleteCampaign.vue";
-import CustomerForm from "./admin/customers/CustomerForm";
-import CustomerIndex from "./admin/customers/CustomerIndex";
-import RoleIndex from "./admin/roles/RoleIndex";
-import RoleForm from "./admin/roles/RoleForm";
+
 import UserIndex from "./admin/users/UserIndex";
 import UserForm from "./admin/users/UserForm";
-import PartnerIndex from "./admin/partners/PartnerIndex";
-import PartnerForm from "./admin/partners/PartnerForm";
-import PermissionIndex from "./admin/permissions/PermissionIndex";
-import PermissionForm from "./admin/permissions/PermissionForm";
-import PermissionAssign from "./admin/permissions/PermissionAssign";
-import CampaignInstallIndex from "./admin/campaigns/campaign_installs/CampaignInstallIndex";
-import CampaignInstallForm from "./admin/campaigns/campaign_installs/CampaignInstallForm";
-import PartnerCampaignIndex from "./admin/campaigns/campaign_partners/PartnerCampaignIndex";
-import PartnerCampaignForm from "./admin/campaigns/campaign_partners/PartnerCampaignForm";
-import DebtSettleForm from "./admin/debt_settle/DebtSettleForm";
-import DebtSettleIndex from "./admin/debt_settle/DebtSettleIndex";
-import StatisticalDetail from "./admin/campaigns/campaign_installs/StatisticalDetail.vue";
-import PayStatistical from "./admin/debt_settle/PayStatistical.vue";
-import PayDetail from "./admin/debt_settle/PayDetail.vue";
-import DebtIndex from "./admin/debt_settle/DebtIndex.vue";
-import DebtForm from "./admin/debt_settle/DebtForm.vue";
+
 
 
 export default {
     DashboardIndex,
-    CampaignForm,
-    CampaignIndex,
-    CampaignStatistical,
-    CampaignStatisticalDetail,
-    DeleteCampaign,
-    CustomerIndex,
-    CustomerForm,
-    RoleIndex,
-    RoleForm,
+
     UserIndex,
     UserForm,
-    PartnerIndex,
-    PartnerForm,
-    PermissionIndex,
-    PermissionForm,
-    PermissionAssign,
-    CampaignInstallIndex,
-    CampaignInstallForm,
-    PartnerCampaignIndex,
-    PartnerCampaignForm,
-    DebtSettleForm,
-    DebtSettleIndex,
-    StatisticalDetail,
-    PayStatistical,
-    PayDetail,
-    DebtIndex,
-    DebtForm
 
 };
